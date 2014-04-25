6e1fa7499e601b944c671a95ad441827a757998a
hicpp <lghtsiho@gmail.com>
2014-04-25 14:07:47 +0800

    添加版权

--- rscode.pas
+++ rscode.pas
@@ -1,11 +1,29 @@
 {*******************************************************************************
 
-    Author: [$]+                                    
-    QQ: 270965332
-    Homepage: http://www.hicpp.com
+ * qrencode - QR Code encoder
+ *
+ * Reed solomon encoder. This code is taken from Kentaro Fukuchi's rscode.h and
+ * rscode.c then editted and packed into a .pas file.
+ * Copyright (C) 2006-2011 Kentaro Fukuchi <kentaro@fukuchi.org>
+ *
+ * Copyright (C) 2014 Hao Shi <admin@hicpp.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
                                                                
     revision history
-      2014-04-14  ¸üÐÂÓÚqrencode-3.4.3
+      2014-04-14  update from qrencode-3.4.3
 
 *******************************************************************************}
 
